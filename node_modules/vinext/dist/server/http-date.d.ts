b7ae336c060b64543a352d919ff4b7c053d52c01
George Miller <george.miller.chess@gmail.com>
2026-09-20 23:49:31 +0100

    Remove dependencies and secrets from repository

--- node_modules/vinext/dist/server/http-date.d.ts
+++
@@ -1,5 +0,0 @@
-//#region src/server/http-date.d.ts
-/** Parse only the three HTTP-date wire formats accepted by RFC 9110. */
-declare function parseHttpDate(value: string): number;
-//#endregion
-export { parseHttpDate };
